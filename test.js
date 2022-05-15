39bbd6daff096bef3892e23fb3aca3318db2583e
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-15 06:13:58 +0200

    test1 pased!

---
+++ test.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+//open test.txt
+const filePath = path.join(__dirname, "test.txt");
+let file = fs.readFileSync(filePath, "utf8");
+
+function process(file) {
+  let aux = [];
+  let blocks;
+
+  pattern = /([a-zíA-Z]+(\s|\n)*)+/g;
+  aux = file.match(pattern);
+
+  return aux;
+}
+
+console.log("------------------------------------------");
+console.log(process(file).length);
+process(file).forEach((element) => {
+  console.log("-------");
+  console.log(element);
+});
